e153b52515cf010171775d63b7eb97836fa62d28
hbx <hbxmail429@gmail.com>
2026-06-12 21:16:33 +0800

    :sparkles: 框架搭建

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { NButton } from 'naive-ui'
+</script>
+
+<template>
+  <div class="bg-blue-500 text-white">Hello HBX</div>
+  <NButton type="primary"> Hello HBX </NButton>
+</template>
