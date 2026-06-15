5856901f245f98de2105de81a485836514a67c4d
hbx <hbxmail429@gmail.com>
2026-06-15 23:02:55 +0800

    Revert ":wrench: 修改 GitHub Pages 部署路径配置"
    This reverts commit 6ef1a8e1ee8ed266f8e05464d2932f7f1de164eb.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hbx286.github.io/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
