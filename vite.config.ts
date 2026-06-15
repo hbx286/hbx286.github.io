6ef1a8e1ee8ed266f8e05464d2932f7f1de164eb
hbx <hbxmail429@gmail.com>
2026-06-15 22:46:49 +0800

    :wrench: 修改 GitHub Pages 部署路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hbx286.github.io/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
